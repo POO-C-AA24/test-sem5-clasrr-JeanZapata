a3a3e272eb66522976a7349bd89a4666c2ff6829
Jean Zapata <jpzapata1@utpl.edu.ec>
2024-05-09 16:01:43 -0500

    Taller 5

---
+++ apTerrenos/src/EjecturoTerreno.java
@@ -0,0 +1,10 @@
+
+public class EjecturoTerreno {
+    public static void main(String[] args) {
+        Terreno terreno1 = new Terreno(40,10 , 2);
+        terreno1.CalcularArea();
+        terreno1.CalcularCostoFinal();
+        System.out.println(terreno1);
+    }
+    
+}
--- apTerrenos/src/Terreno.java
+++ apTerrenos/src/Terreno.java
@@ -1,7 +1,31 @@
-public class Terreno{
+public class Terreno {
     public double alto; 
     public double ancho;
     public double vmc;
     public double area;
     public double costoFinal;
+
+    public Terreno(double alto, double ancho, double vmc) {
+        this.alto = alto;
+        this.ancho = ancho;
+        this.vmc = vmc;
+    }
+
+    public void CalcularArea(){
+        this.area = this.ancho * this.alto;
+    }
+
+    public void CalcularCostoFinal(){
+        costoFinal = area * vmc;
+        if (area > 300) {
+            costoFinal *= 0.8;
+        } else if (area >= 200) {
+            costoFinal *= 0.9; 
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Terreno{" + "alto=" + alto + ", ancho=" + ancho + ", vmc=" + vmc + ", area=" + area + ", costoFinal=" + costoFinal + '}';
+    }
 }
